c58a7238e67e8f90512e5a275b6dbbbf8989c106
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-04-20 18:24:07 +0530

    Create reduce.js

---
+++ Method Demonstrations/Array-Methods/JS-reduce-method/reduce.js
@@ -0,0 +1,12 @@
+const total = amounts
+    .reduce((accumulator, value) => (accumulator += value), 0)
+    .toFixed(2);
+  const income = amounts
+    .filter((value) => value > 0)
+    .reduce((accumulator, value) => (accumulator += value), 0)
+    .toFixed(2);
+  const expense = (
+    amounts
+      .filter((value) => value < 0)
+      .reduce((accumulator, value) => (accumulator += value), 0) * -1
+  ).toFixed(2);
