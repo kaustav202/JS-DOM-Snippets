92cea84495a4391ae9401239ca85f5f6958364d4
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-04-28 18:03:49 +0530

    Update call.js

--- Advanced Functions/indirect-function-calls/call/call.js
+++ Advanced Functions/indirect-function-calls/call/call.js
@@ -1,13 +1,13 @@
+firstName: 'Ann',
+lastName: 'Brown'
+
 let p1 = {
   firstName: 'John',
   lastName: 'Smith'
 };
-let p2 = {
-  firstName: 'Ann',
-  lastName: 'Brown'
-};
+
 function sayWelcome(greeting) {
   console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
 }
 sayWelcome.call(p1, 'Welcome'); // Welcome John Smith
-sayWelcome.call(p2, 'Welcome'); // Welcome Ann Brown
+sayWelcome.call(this , 'Welcome'); // Welcome Ann Brown
