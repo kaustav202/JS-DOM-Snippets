e33d2aae6780e10b4da1ff903dd282e4a1af8e54
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-05-14 10:14:23 +0530

    Create clone-dom-node.js

---
+++ Tricks/clone-dom-node.js
@@ -0,0 +1,7 @@
+const el = document.querySelector('.new-element');
+
+const clone = el.cloneNode(true);
+
+/* create a deepclone of any domnode  */
+
+/* No need to create same element and set all the properies again and again */
